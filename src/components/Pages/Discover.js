31c3c06fda2687987d6e98364f625d7c413b860c
JD <janelle.m.deane@gmail.com>
2020-11-05 13:01:35 -0800

    about page and initial discover page

--- src/components/Pages/Discover.js
+++ src/components/Pages/Discover.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 export default class Discover extends Component {
-    state ={
-        img:"",
+    state = {
+        img: "",
         match: false,
         friendsCount: 0
     }
@@ -11,8 +11,29 @@ export default class Discover extends Component {
     render() {
         return (
             <div>
-            <h2>Discover</h2>
-         </div>
+                <h2>Make New Pup Friends!</h2>
+                <h3>Thumbs up on any pups you'd like to meet!</h3>
+
+                <div className="card">
+                    <div className="img-container">
+                        <img alt={props.name} src={props.image} />
+                    </div>
+                    <div className="content">
+                        <ul>
+                            <li>
+                                <strong>Name:</strong> {props.name}
+                            </li>
+                            <li>
+                                <strong>Occupation:</strong> {props.occupation}
+                            </li>
+                            <li>
+                                <strong>Location:</strong> {props.location}
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
         )
     }
 }
